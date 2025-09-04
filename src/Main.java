4377b3479238c034e41b2816a5e09a1edfa9678b
sebasmoren13 <sebasmoren13@gmail.com>
2025-09-04 17:19:54 -0500

    prueba de aleatorios

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-Scanner teclado = new Scanner(System.in);
-int n1,n2,suma;
+        /*Scanner teclado = new Scanner(System.in);
+        int n1, n2, suma;
 
-System.out.println("Ingrese el numero 1:");
-n1 = teclado.nextInt();
-System.out.println("Ingrese el numero 2:");
-n2 = teclado.nextInt();
-suma = n1 + n2;
-System.out.println("El resultado es: " + suma);
+        System.out.println("Ingrese el numero 1:");
+        n1 = teclado.nextInt();
+        System.out.println("Ingrese el numero 2:");
+        n2 = teclado.nextInt();
+        suma = n1 + n2;
+        System.out.println("El resultado es: " + suma);*/
 
+
+        Random aleatorio = new Random();
+        int a, b, z;
+        System.out.println("Numero generado");
+        a = aleatorio.nextInt(51);
+        System.out.println("Segundo Numero ");
+        b = aleatorio.nextInt((30 - 5) + 1) + 5;
+
+    z=a+b;
+        System.out.println("El resultado es: " + z);
     }
 }

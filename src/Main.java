6c11e9582ec2662870e8c23bf5d7adc5025dc708
sebasmoren13 <sebasmoren13@gmail.com>
2025-09-04 17:55:57 -0500

    Prueba de dos dados

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("El resultado es: " + suma);*/
 
 
-        Random aleatorio = new Random();
+        /*Random aleatorio = new Random();
         int a, b, z;
         System.out.println("Numero generado");
         a = aleatorio.nextInt(51);
@@ -20,6 +20,37 @@ public class Main {
         b = aleatorio.nextInt((30 - 5) + 1) + 5;
 
     z=a+b;
-        System.out.println("El resultado es: " + z);
+        System.out.println("El resultado es: " + z);*/
+
+
+        Random aleatorio = new Random();
+        int a,b,sumaD1=0,sumaD2=0;
+            for (int i=1;i<=20; i++) {
+                System.out.println("Numero aleatorio dado 1");
+                a =aleatorio.nextInt((6 - 1) + 1) + 1;
+                sumaD1+=a;
+
+                System.out.println("Numero aleatorio dado 2");
+                b = aleatorio.nextInt((6 - 1) + 1) +1;
+                sumaD2+=b;
+            }
+
+            if(sumaD1<sumaD2){
+                System.out.println("El dado 1 gana="+sumaD1);
+
+            }
+            else if(sumaD1>sumaD2){
+                System.out.println("El dado 2 gana="+sumaD2);
+        }
+                else if(sumaD1==sumaD2){
+                    System.out.println("EMPATE="+sumaD2);
+            }
+
+
+
+
+
+
+
     }
 }

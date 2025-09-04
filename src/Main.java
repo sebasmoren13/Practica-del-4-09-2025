43ba265bff9e7b1b561de51ec9c1641dfda41dd0
sebasmoren13 <sebasmoren13@gmail.com>
2025-09-04 17:08:35 -0500

    Prueba de suma con Scanner

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+Scanner teclado = new Scanner(System.in);
+int n1,n2,suma;
+
+System.out.println("Ingrese el numero 1:");
+n1 = teclado.nextInt();
+System.out.println("Ingrese el numero 2:");
+n2 = teclado.nextInt();
+suma = n1 + n2;
+System.out.println("El resultado es: " + suma);
+
+    }
+}
